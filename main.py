b11b8858435505014f2707c64809afb006162a70
Johannes Kopeczky <johannes87@gmail.com>
2021-10-18 14:34:10 +0200

    Layout fixes

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ timer_id = ''
 camera_dict = {}
 api_url = "https://data.goteborg.se/TrafficCamera/v1.0/TrafficCameras/2045abfc-4065-4741-a580-1755cbe3e245?format=json"
 
-class Users:
-    def __init__(self):
-        pass
-
 
 class ApiCon:
     def __init__(self):
@@ -23,12 +19,17 @@ class ApiCon:
         else:
             print("Error")
 
-    def get_camera(self, camera_name):
+    def get_camera_image(self, camera_name):
         if camera_name in camera_dict:
             r = requests.get(camera_dict.get(camera_name))
             return r
         else:
             return None
+
+
+class ApiWindow:
+    def __init__(self):
+        pass
 
 
 class MainWindow:
@@ -55,9 +56,12 @@ class MainWindow:
         self.dropdown = tk.OptionMenu(self.window, self.clicked, *camera_dict.keys(), command=self.show_camera)
         self.dropdown.place(x=54, y=600)
 
+        self.api_button = tk.Button(self.window, text="Manage API-key", height=1, width=13, command=self.manage_api_button)
+        self.api_button.place(x=560, y=600)
+
         self.countdown_text = tk.StringVar()
         self.countdown_label = tk.Label(text="")
-        self.countdown_label.place(x=600, y=600)
+        self.countdown_label.place(x=54, y=560)
 
         self.close_button = tk.Button(self.window, text="Close", height=1, width=8, command=self.window.destroy)
         self.close_button.place(x=680, y=600)
@@ -66,13 +70,13 @@ class MainWindow:
 
     def show_camera(self, x):
         choice = self.clicked.get()
-        self.returned_image = api_con.get_camera(choice)
+        self.returned_image = api_con.get_camera_image(choice)
         self.image_data = self.returned_image.content
         self.image_data_processed = ImageTk.PhotoImage(Image.open(BytesIO(self.image_data)))
         self.image_placeholder.configure(image=self.image_data_processed)
         self.image_placeholder.pack()
 
-        self.countdown(10)
+        self.countdown(45)
         self.window.mainloop()
 
     def countdown(self, count):
@@ -87,8 +91,10 @@ class MainWindow:
             timer_id = self.window.after(1000, self.countdown, count-1)
         elif count == 0:
             #Uppdatera bilden
-            pass
+            self.show_camera(self)
 
+    def manage_api_button(self):
+        pass
 
 api_con = ApiCon()
 main = MainWindow()
